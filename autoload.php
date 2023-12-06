d9fb931d73deb92d8dddae66ab6468c0e147bbdd
Mitantsoaa <mitaranaivo@gmail.com>
2023-12-06 06:43:53 +0300

    Correction namaspace

--- autoload.php
+++ autoload.php
@@ -1,5 +1,14 @@
-
 <?php
+namespace MonProjet;
 spl_autoload_register(function($class){
-  require_once $class . '.php';
+    $paths = array(
+        join(DIRECTORY_SEPARATOR, [__DIR__]),
+        join(DIRECTORY_SEPARATOR, ["MonProjet".__DIR__]),
+    );
+
+    foreach($paths as $path){
+        $file = join(DIRECTORY_SEPARATOR, [$path, $class.'.php']) ;
+        if(file_exists($file))
+            return require_once $file;
+    }
 });
